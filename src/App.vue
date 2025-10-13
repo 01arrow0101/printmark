6075dc85b6e1e4a13ab45688161618f3bfe71fb8
AlexeyBezuglyDeveloper <112628319+01arrow0101@users.noreply.github.com>
2025-10-13 21:36:00 +0300

    Add files via upload
    add start files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/icons/PrintMarkHeader.vue'
+</script>
+
+<template>
+  <header>
+    <Header />
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped></style>
